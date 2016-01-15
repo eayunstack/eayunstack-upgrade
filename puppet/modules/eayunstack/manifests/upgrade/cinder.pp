8cec13c991b751004f25405ab1e89ceca64e90ca
Zhao Chao <zhaochao1984@gmail.com>
2016-01-15 18:13:37 +0800

    cinder: eayunstack v1.1
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>

---
+++ puppet/modules/eayunstack/manifests/upgrade/cinder.pp
@@ -0,0 +1,64 @@
+class eayunstack::upgrade::cinder (
+  $fuel_settings,
+) {
+  if $eayunstack_node_role == 'controller' {
+
+    $cinder_packages = [
+      'openstack-cinder', 'python-cinder', 'python-cinderclient',
+    ]
+    package { $cinder_packages:
+      ensure => latest,
+    }
+
+    $cinder_services = [
+      'openstack-cinder-api',
+      'openstack-cinder-scheduler',
+      'openstack-cinder-volume',
+    ]
+    service { $cinder_services:
+      ensure => running,
+      enable => true,
+    }
+
+    exec {'cinder-db-sync':
+      command     => 'cinder-manage db sync',
+      path        => '/usr/bin',
+      user        => 'cinder',
+      refreshonly => true,
+      tries       => 3,
+      try_sleep   => 20,
+    }
+
+    Package['openstack-cinder'] {
+      notify => [
+        Exec['cinder-db-sync'],
+        Service['openstack-cinder-api'],
+        Service['openstack-cinder-scheduler'],
+        Service['openstack-cinder-volume'],
+      ]
+    }
+
+    Exec['cinder-db-sync'] {
+      notify => [
+        Service['openstack-cinder-api'],
+        Service['openstack-cinder-scheduler'],
+        Service['openstack-cinder-volume'],
+      ]
+    }
+
+    Service['openstack-cinder-api'] {
+      before => [
+        Service['openstack-cinder-scheduler'],
+        Service['openstack-cinder-volume'],
+      ]
+    }
+  } else {
+
+    $cinder_packages = [
+      'python-cinder', 'python-cinderclient',
+    ]
+    package { $cinder_packages:
+      ensure => latest,
+    }
+  }
+}
