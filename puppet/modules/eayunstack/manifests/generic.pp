eba09270572effc9f73d00a7539ad98fa98ea9ec
Zhao Chao <zhaochao1984@gmail.com>
2016-03-19 00:06:14 +0800

    move service array to a generic pp.
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
    (cherry picked from commit 938540fc3af381ba311f5782b3ee32f1f292c72f)
    Signed-off-by: huntxu <mhuntxu@gmail.com>

---
+++ puppet/modules/eayunstack/manifests/generic.pp
@@ -0,0 +1,37 @@
+class eayunstack::generic {
+
+  $openstack_services = {
+    'controller' => [
+      # Ceilometer
+      'openstack-ceilometer-alarm-notifier', 'openstack-ceilometer-api',
+      'openstack-ceilometer-collector', 'openstack-ceilometer-notification',
+      'openstack-ceilometer-central', 'openstack-ceilometer-alarm-evaluator',
+      # Cinder
+      'openstack-cinder-api', 'openstack-cinder-scheduler',
+      'openstack-cinder-volume',
+      # Glance
+      'openstack-glance-api', 'openstack-glance-registry',
+      # Heat
+      'openstack-heat-api-cfn', 'openstack-heat-api-cloudwatch',
+      'openstack-heat-api', 'openstack-heat-engine',
+      # Keystone
+      'openstack-keystone',
+      # Neutron
+      'neutron-server', 'neutron-qos-agent', 'neutron-metering-agent',
+      'neutron-openvswitch-agent', 'neutron-l3-agent', 'neutron-dhcp-agent',
+      'neutron-metadata-agent', 'neutron-lbaas-agent',
+      # Nova
+      'openstack-nova-api', 'openstack-nova-cert', 'openstack-nova-conductor',
+      'openstack-nova-consoleauth', 'openstack-nova-novncproxy',
+      'openstack-nova-objectstore', 'openstack-nova-scheduler',
+    ],
+    'compute' => [
+      # Ceilometer
+      'openstack-ceilometer-compute',
+      # Neutron
+      'neutron-openvswitch-agent', 'neutron-qos-agent',
+      # Nova
+      'openstack-nova-compute',
+    ],
+  }
+}
--- puppet/modules/eayunstack/manifests/upgrade/oslo/messaging.pp
+++ puppet/modules/eayunstack/manifests/upgrade/oslo/messaging.pp
@@ -11,47 +11,13 @@ class eayunstack::upgrade::oslo::messaging (
 
   if $eayunstack_node_role == 'controller' {
 
-    $all_related_services = [
-      # Ceilometer
-      'openstack-ceilometer-alarm-notifier', 'openstack-ceilometer-api',
-      'openstack-ceilometer-collector', 'openstack-ceilometer-notification',
-      'openstack-ceilometer-central', 'openstack-ceilometer-alarm-evaluator',
-      # Cinder
-      'openstack-cinder-api', 'openstack-cinder-scheduler',
-      'openstack-cinder-volume',
-      # Glance
-      'openstack-glance-api', 'openstack-glance-registry',
-      # Heat
-      'openstack-heat-api-cfn', 'openstack-heat-api-cloudwatch',
-      'openstack-heat-api', 'openstack-heat-engine',
-      # Keystone
-      'openstack-keystone',
-      # Neutron
-      'neutron-server', 'neutron-qos-agent', 'neutron-metering-agent',
-      'neutron-openvswitch-agent', 'neutron-l3-agent', 'neutron-dhcp-agent',
-      'neutron-metadata-agent', 'neutron-lbaas-agent',
-      # Nova
-      'openstack-nova-api', 'openstack-nova-cert', 'openstack-nova-conductor',
-      'openstack-nova-consoleauth', 'openstack-nova-novncproxy',
-      'openstack-nova-objectstore', 'openstack-nova-scheduler',
-    ]
-
     Package['python-oslo-messaging'] ~>
-      Service[$all_related_services]
+      Service[$::eayunstack::generic::openstack_services['controller']]
 
   } elsif $eayunstack_node_role == 'compute' {
 
-    $all_related_services = [
-      # Ceilometer
-      'openstack-ceilometer-compute',
-      # Neutron
-      'neutron-openvswitch-agent', 'neutron-qos-agent',
-      # Nova
-      'openstack-nova-compute',
-    ]
-
     Package['python-oslo-messaging'] ~>
-      Service[$all_related_services]
+      Service[$::eayunstack::generic::openstack_services['compute']]
 
     # There is no service on ceph-osd to be restarted.
   }
