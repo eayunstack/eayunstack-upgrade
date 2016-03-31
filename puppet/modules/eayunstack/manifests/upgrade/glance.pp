d3973367eb9c75b21826a275e3e1a965c5f58b47
fabian4 <cybing4@gmail.com>
2016-03-31 17:24:15 +0800

    update glance notification_driver default driver is noon
    Signed-off-by: fabian <cybing4@gmail.com>

--- puppet/modules/eayunstack/manifests/upgrade/glance.pp
+++ puppet/modules/eayunstack/manifests/upgrade/glance.pp
@@ -4,6 +4,35 @@ class eayunstack::upgrade::glance (
 
   if $::eayunstack_node_role == 'controller' {
 
+    $glance_packages = [
+      'python-glance', 'python-glanceclient',
+      'python-glance-store', 'openstack-glance',
+    ]
+    package { $glance_packages:
+      ensure => latest,
+    }
+
+    augeas { 'glance-api':
+      context => '/files/etc/glance/glance-api.conf',
+      lens => 'Puppet.lns',
+      incl => '/etc/glance/glance-api.conf',
+      changes => [
+        "set DEFAULT/notification_driver messaging",
+      ],
+      require => Package['openstack-glance'],
+      notify => Service['openstack-glance-api'],
+    }
+
+    augeas { 'glance-registry':
+      context => '/files/etc/glance/glance-registry.conf',
+      lens => 'Puppet.lns',
+      incl => '/etc/glance/glance-registry.conf',
+      changes => [
+        "set DEFAULT/notification_driver messaging",
+      ],
+      require => Package['openstack-glance'],
+      notify => Service['openstack-glance-registry'],
+    }
     $systemd_services = [
       'openstack-glance-api', 'openstack-glance-registry',
     ]
@@ -12,6 +41,11 @@ class eayunstack::upgrade::glance (
       ensure => running,
       enable => true,
     }
+
+    Package['openstack-glance'] ~>
+      Service['openstack-glance-registry']
+    Package['openstack-glance'] ~>
+      Service['openstack-glance-api']
 
   }
   # There is nothing to do on ceph-osd or compute.
