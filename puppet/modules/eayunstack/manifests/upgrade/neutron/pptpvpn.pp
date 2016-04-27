77ba74ee8dc5eda7f334da4ece3bf544a3db868c
huntxu <mhuntxu@gmail.com>
2016-04-27 12:20:28 +0800

    neutron pptpvpn: only apply resources for eayunstack 1.1
    Related PR: eayunstack/EayunStack-building#36
    
    Signed-off-by: huntxu <mhuntxu@gmail.com>

--- puppet/modules/eayunstack/manifests/upgrade/neutron/pptpvpn.pp
+++ puppet/modules/eayunstack/manifests/upgrade/neutron/pptpvpn.pp
@@ -1,67 +1,75 @@
 class eayunstack::upgrade::neutron::pptpvpn {
 
-  $packages = ['pptpd']
-  package { $packages:
-    ensure => latest,
-  }
+  $package_name = "openstack-neutron"
+  $required_release = "19"  # Eayunstack-building PR #36
+  $available_release = get_eayunstack_pkg_rel($package_name)
 
-  augeas { 'add-pptp-vpn-service-provider':
-    context => '/files/etc/neutron/neutron.conf',
-    lens => 'Puppet.lns',
-    incl => '/etc/neutron/neutron.conf',
-    changes => [
-      'set service_providers/service_provider[last()+1] VPN:pptp:neutron.services.vpn.service_drivers.pptp.PPTPVPNDriver'
-    ],
-    onlyif => 'match service_providers/service_provider[.="VPN:pptp:neutron.services.vpn.service_drivers.pptp.PPTPVPNDriver"] size < 1',
-  }
+  if versioncmp($available_release, $required_release) >= 0 {
 
-  augeas { 'add-pptp-vpn-device-driver':
-    context => '/files/etc/neutron/l3_agent.ini',
-    lens => 'Puppet.lns',
-    incl => '/etc/neutron/l3_agent.ini',
-    changes => [
-      'set vpnagent/vpn_device_driver[last()+1] neutron.services.vpn.device_drivers.pptp.PPTPDriver'
-    ],
-    onlyif => 'match vpnagent/vpn_device_driver[.="neutron.services.vpn.device_drivers.pptp.PPTPDriver"] size < 1',
-  }
+    $packages = ['pptpd']
+    package { $packages:
+      ensure => latest,
+    }
 
-  file { 'ppp-dir':
-    ensure => directory,
-    path => '/etc/ppp',
-    owner => 'neutron',
-    group => 'root',
-  }
+    augeas { 'add-pptp-vpn-service-provider':
+      context => '/files/etc/neutron/neutron.conf',
+      lens => 'Puppet.lns',
+      incl => '/etc/neutron/neutron.conf',
+      changes => [
+        'set service_providers/service_provider[last()+1] VPN:pptp:neutron.services.vpn.service_drivers.pptp.PPTPVPNDriver'
+      ],
+      onlyif => 'match service_providers/service_provider[.="VPN:pptp:neutron.services.vpn.service_drivers.pptp.PPTPVPNDriver"] size < 1',
+    }
 
-  file { 'ppp-chap-secrets':
-    ensure => file,
-    path => '/etc/ppp/chap-secrets',
-    mode => '0644',
-    owner => 'neutron',
-    group => 'neutron',
-  }
+    augeas { 'add-pptp-vpn-device-driver':
+      context => '/files/etc/neutron/l3_agent.ini',
+      lens => 'Puppet.lns',
+      incl => '/etc/neutron/l3_agent.ini',
+      changes => [
+        'set vpnagent/vpn_device_driver[last()+1] neutron.services.vpn.device_drivers.pptp.PPTPDriver'
+      ],
+      onlyif => 'match vpnagent/vpn_device_driver[.="neutron.services.vpn.device_drivers.pptp.PPTPDriver"] size < 1',
+    }
 
-  $ip_local_files = ['ip-up.local', 'ip-down.local']
-  file { $ip_local_files:
-    ensure => file,
-    mode => '0755',
-    owner => 'root',
-    group => 'root',
-  }
-  File['ip-up.local'] {
-    path => '/etc/ppp/ip-up.local',
-    source => 'puppet:///modules/eayunstack/ip-up.local',
-  }
-  File['ip-down.local'] {
-    path => '/etc/ppp/ip-down.local',
-    source => 'puppet:///modules/eayunstack/ip-down.local',
-  }
+    file { 'ppp-dir':
+      ensure => directory,
+      path => '/etc/ppp',
+      owner => 'neutron',
+      group => 'root',
+    }
 
-  Package['openstack-neutron-ml2'] ->
-    Augeas['add-pptp-vpn-service-provider'] ~>
-      Service['neutron-server']
+    file { 'ppp-chap-secrets':
+      ensure => file,
+      path => '/etc/ppp/chap-secrets',
+      mode => '0644',
+      owner => 'neutron',
+      group => 'neutron',
+    }
 
-  Package['openstack-neutron-vpn-agent'] ->
-    Augeas['add-pptp-vpn-device-driver'] ~>
-      Service['neutron-l3-agent']
+    $ip_local_files = ['ip-up.local', 'ip-down.local']
+    file { $ip_local_files:
+      ensure => file,
+      mode => '0755',
+      owner => 'root',
+      group => 'root',
+    }
+    File['ip-up.local'] {
+      path => '/etc/ppp/ip-up.local',
+      source => 'puppet:///modules/eayunstack/ip-up.local',
+    }
+    File['ip-down.local'] {
+      path => '/etc/ppp/ip-down.local',
+      source => 'puppet:///modules/eayunstack/ip-down.local',
+    }
+
+    Package['openstack-neutron-ml2'] ->
+      Augeas['add-pptp-vpn-service-provider'] ~>
+        Service['neutron-server']
+
+    Package['openstack-neutron-vpn-agent'] ->
+      Augeas['add-pptp-vpn-device-driver'] ~>
+        Service['neutron-l3-agent']
+
+  }
 
 }
