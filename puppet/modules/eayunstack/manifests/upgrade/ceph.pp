5dd224106c7561365e45b64c9ae482d0f424f1b1
blkart <blkart.org@gmail.com>
2016-05-18 13:40:56 +0800

    reset ceph auth for nova user (#31)
    For the optimization of create snapshot from instance, nova user(client.compute) needs to have write access to the images pool.
    
    Signed-off-by: blkart <blkart.org@gmail.com>

---
+++ puppet/modules/eayunstack/manifests/upgrade/ceph.pp
@@ -0,0 +1,16 @@
+class eayunstack::upgrade::ceph (
+  $fuel_settings,
+) {
+  if $eayunstack_node_role == 'controller' {
+    # just get current mon acl, without any modification
+    $ceph_mon_caps = get_ceph_auth_info('client.compute', 'mon')
+    # modify user 'compute''s access permission from 'rx' to 'rwx' for images pool
+    $ceph_osd_caps = modify_ceph_auth_info('client.compute', 'osd', 'allow rx pool=images', 'allow rwx pool=images')
+    exec {'reset-cephx-acl-for-user-compute':
+      path => '/usr/bin/',
+      command => "ceph auth caps client.compute mon '${ceph_mon_caps}' osd '${ceph_osd_caps}'",
+      tries       => 10,
+      try_sleep   => 20,
+    }
+  }
+}
