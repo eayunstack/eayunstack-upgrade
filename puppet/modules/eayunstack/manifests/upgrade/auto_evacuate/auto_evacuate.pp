eayunstack/eayunstack-upgrade

commit d33ebb07c73c4fcce67c1cc4b126e33ca840be75

Add eayunstack autoevacuate base cluster consul configure, Add consul install 3 controll node
Signed-off-by: zhouyang1222 <419606291@qq.com>

puppet/modules/eayunstack/manifests/upgrade/auto_evacuate/auto_evacuate.pp
@@ -1,0 +1,73 @@
+class eayunstack::upgrade::auto_evacuate::auto_evacuate (
+  $fuel_settings,
+) {
+  $nodes = $fuel_settings['nodes']
+  $node = filter_nodes($nodes, 'role', 'controller')
+  $first_node = filter_nodes($nodes, 'role', 'primary-controller')
+  $storage_ip1 = $first_node[0]['storage_address']
+  $storage_ip2 = $node[0]['storage_address']
+  $storage_ip3 = $node[1]['storage_address']
+  $host_names = [$first_node[0]['fqdn'], $node[0]['fqdn'], $node[1]['fqdn']]
+  $host_name = $fuel_settings['fqdn']
+  $local_ips = filter_nodes($nodes, 'fqdn', $host_name)
+  $storage_ip = $local_ips[0]['storage_address']
+  $auth_url = "http://${fuel_settings['management_vip']}:5000/v2.0"
+
+  $packages = ['consul', 'python-consul', 'eayunstack-auto-evacuate']
+
+  if $eayunstack_node_role == 'controller' {
+    if $host_name in $host_names {
+      package { $packages:
+        ensure => latest,
+      }
+
+      file { 'consul_config_dictory':
+        ensure  => directory,
+        path    => '/etc/consul/storage',
+        require => Package['consul'],
+      }
+
+      file { 'consul_config_file':
+        ensure  => file,
+        path    => '/etc/consul/storage/consul.json',
+        require => File['consul_config_dictory'],
+        content => template('eayunstack/consul.erb'),
+      }
+
+      file { 'consul_sysconfig_file':
+        ensure  => file,
+        path    => '/etc/sysconfig/consul',
+        require => Package['consul'],
+        content => 'CMD_OPTS="agent -config-dir=/etc/consul/storage -rejoin"',
+      }
+
+      file { 'consul_lib_dictory':
+        ensure  => directory,
+        path    => '/var/lib/consul',
+        require => Package['consul'],
+        owner   => 'consul',
+        group   => 'consul',
+      }
+
+    augeas { 'evacuate_config_file':
+        context => '/files/etc/autoevacuate/evacuate.conf',
+        lens    => 'Puppet.lns',
+        incl    => '/etc/autoevacuate/evacuate.conf',
+        changes => "set novaclient/auth_url ${auth_url}",
+        require => Package['eayunstack-auto-evacuate'],
+      }
+
+      service { 'consul':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['consul_config_file', 'consul_sysconfig_file', 'consul_lib_dictory'],
+      }
+
+      service { 'eayunstack-auto-evacuate':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['evacuate_config_file'],
+      }
+    }
+  }
+}

puppet/modules/eayunstack/manifests/upgrade.pp
@@ -3,6 +3,9 @@ class eayunstack::upgrade (
 ) {
   include eayunstack::generic
 
+  class { 'eayunstack::upgrade::auto_evacuate::auto_evacuate':
+    fuel_settings => $fuel_settings,
+  }
   class { 'eayunstack::upgrade::ceilometer::ceilometer':
     fuel_settings => $fuel_settings,
   }

puppet/modules/eayunstack/templates/consul.erb
@@ -1,0 +1,14 @@
+{
+        "datacenter": "br_storage",
+        "data_dir": "/var/lib/consul",
+        "log_level": "INFO",
+        "node_name": "<%= @host_name%>",
+        "server": true,
+        "bootstrap_expect": 3,
+        "ui_dir": "/opt/dist",
+        "bind_addr": "<%= @storage_ip%>",
+        "client_addr": "<%= @storage_ip%>",
+        "start_join": ["<%= @storage_ip1%>", "<%= @storage_ip2%>", "<%= @storage_ip3%>"],
+        "enable_syslog": true
+}
+

commit 0f231b0d299ed54aaa9de44887cf112903522646

Sync #44 from devel to testing
Fix eayunstack-auto-evacuate dependencies

Fixes: d33ebb0 ("Add eayunstack autoevacuate base clusterd...")

Signed-off-by: Hunt Xu <mhuntxu@gmail.com>
(cherry picked from commit 024222f1f927b14e5f1bbc6a1841e2ed3488bd64)
Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

puppet/modules/eayunstack/manifests/upgrade/auto_evacuate/auto_evacuate.pp
@@ -13,7 +13,7 @@ class eayunstack::upgrade::auto_evacuate::auto_evacuate (
   $storage_ip = $local_ips[0]['storage_address']
   $auth_url = "http://${fuel_settings['management_vip']}:5000/v2.0"
 
-  $packages = ['consul', 'python-consul', 'eayunstack-auto-evacuate']
+  $packages = ['consul', 'eayunstack-auto-evacuate']
 
   if $eayunstack_node_role == 'controller' {
     if $host_name in $host_names {
@@ -22,31 +22,27 @@ class eayunstack::upgrade::auto_evacuate::auto_evacuate (
       }
 
       file { 'consul_config_dictory':
-        ensure  => directory,
-        path    => '/etc/consul/storage',
-        require => Package['consul'],
+        ensure => directory,
+        path   => '/etc/consul/storage',
       }
 
       file { 'consul_config_file':
         ensure  => file,
         path    => '/etc/consul/storage/consul.json',
-        require => File['consul_config_dictory'],
         content => template('eayunstack/consul.erb'),
       }
 
       file { 'consul_sysconfig_file':
         ensure  => file,
         path    => '/etc/sysconfig/consul',
-        require => Package['consul'],
         content => 'CMD_OPTS="agent -config-dir=/etc/consul/storage -rejoin"',
       }
 
       file { 'consul_lib_dictory':
-        ensure  => directory,
-        path    => '/var/lib/consul',
-        require => Package['consul'],
-        owner   => 'consul',
-        group   => 'consul',
+        ensure => directory,
+        path   => '/var/lib/consul',
+        owner  => 'consul',
+        group  => 'consul',
       }
 
     augeas { 'evacuate_config_file':
@@ -54,19 +50,16 @@ class eayunstack::upgrade::auto_evacuate::auto_evacuate (
         lens    => 'Puppet.lns',
         incl    => '/etc/autoevacuate/evacuate.conf',
         changes => "set novaclient/auth_url ${auth_url}",
-        require => Package['eayunstack-auto-evacuate'],
       }
 
       service { 'consul':
-        ensure    => running,
-        enable    => true,
-        subscribe => File['consul_config_file', 'consul_sysconfig_file', 'consul_lib_dictory'],
+        ensure => running,
+        enable => true,
       }
 
       service { 'eayunstack-auto-evacuate':
-        ensure    => running,
-        enable    => true,
-        subscribe => Augeas['evacuate_config_file'],
+        ensure => running,
+        enable => true,
       }
 
       firewall { '830 consul port':
@@ -74,6 +67,21 @@ class eayunstack::upgrade::auto_evacuate::auto_evacuate (
         proto  => 'tcp',
         action => 'accept',
       }
+
+      Package['consul'] ->
+        File['consul_config_dictory'] ->
+          File['consul_config_file'] ~>
+            Service['consul']
+
+      Package['consul'] -> File['consul_sysconfig_file'] ~> Service['consul']
+      Package['consul'] -> File['consul_lib_dictory'] -> Service['consul']
+      Package['consul'] ~> Service['consul']
+
+      Package['eayunstack-auto-evacuate'] ->
+        Augeas['evacuate_config_file'] ~>
+          Service['eayunstack-auto-evacuate']
+
+      Package['eayunstack-auto-evacuate'] ~> Service['eayunstack-auto-evacuate']
     }
   }
 }