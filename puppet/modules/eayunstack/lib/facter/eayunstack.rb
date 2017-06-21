653fdf74704902235ed3ede3184e4649a8983336
blkart <blkart.org@gmail.com>
2017-06-21 16:46:22 +0800

    get novncproxy_base_url from environment configuration file
    Signed-off-by: blkart <blkart.org@gmail.com>

--- puppet/modules/eayunstack/lib/facter/eayunstack.rb
+++ puppet/modules/eayunstack/lib/facter/eayunstack.rb
@@ -2,6 +2,7 @@ require 'facter'
 
 node_list_file = '/.eayunstack/node-list'
 node_role_file = '/.eayunstack/node-role'
+env_settings_yaml = '/.eayunstack_env.yaml'
 
 if File.exist?(node_list_file)
   Facter.add('eayunstack_node_info_list') do
@@ -12,5 +13,11 @@ end
 if File.exist?(node_role_file)
   Facter.add('eayunstack_node_role') do
     setcode { File.read(node_role_file).strip }
+  end
+end
+
+if File.exist?(env_settings_yaml)
+  Facter.add('env_settings_yaml') do
+    setcode { File.read(env_settings_yaml) }
   end
 end
