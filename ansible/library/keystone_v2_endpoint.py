eayunstack/eayunstack-upgrade

commit ba37679fa2556f263c51463831fda6de9d7f6797

ansible: add tasks for Ceph configuration updating.
Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>

ansible/ceph.yml
@@ -15,6 +15,8 @@
 
 - name: gather facts and install packages
 
+  vars:
+    with_controllers: "{{ ('controller' in groups) and (groups['controller']|length > 0) }}"
   hosts:
     - controller
     - ceph-osd
@@ -22,6 +24,23 @@
     - obs
 
   become: True
+
+  pre_tasks:
+    - name: fetch fuel facts from the first controller
+      fetch:
+        src: "{{ fuel_facts_file }}"
+        dest: "{{ fuel_facts_fetch_directory }}"
+      run_once: True
+      delegate_to: "{{ groups['controller'][0] }}"
+      register: fuel_facts_fetched
+      when: with_controllers|bool
+
+    - name: load fuel facts on obs nodes
+      include_vars:
+        file: "{{ fuel_facts_fetch_directory }}/{{ groups['controller'][0] }}{{ fuel_facts_file }}"
+      when:
+        - with_controllers|bool
+        - inventory_hostname in groups['obs']
 
   roles:
     - { role: ceph, action: 'install_packages' }

ansible/group_vars/all.yml
@@ -4,6 +4,10 @@ eayunstack_config_base: "/etc/eayunstack/docker"
 eayunstack_log_base: "/var/log/eayunstack/docker"
 eayunstack_passwords_file: "{{ eayunstack_config_base }}/passwords.yml"
 
+# Fuel
+fuel_facts_fetch_directory: fuel_facts
+fuel_facts_file: /etc/astute.yaml
+
 # HAProxy config base directory
 haproxy_config_base: "/etc/haproxy/conf.d"
 

ansible/roles/ceph/defaults/main.yml
@@ -1,4 +1,5 @@
 ceph_cluster: ceph
+ceph_conf_file: "/etc/ceph/ceph.conf"
 
 mon_group_name: mons
 osd_group_name: osds
@@ -22,3 +23,36 @@ ceph_lib_packages:
 
 ceph_rgw_packages:
   - ceph-radosgw
+
+keystone_accepted_roles: "Member, _member_, admin, SwiftOperator"
+keystone_token_cache_size: "500"
+keystone_revocation_interval: "500"
+rgw_s3_use_keystone: "true"
+
+ceph_conf:
+  - section: "global"
+    configurations:
+      - option: "rgw keystone url"
+        value: "http://{{ management_vip }}:{{ keystone_admin_port }}"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"
+      - option: "rgw keystone admin token"
+        value: "{{ keystone['admin_token'] }}"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"
+      - option: "rgw keystone accepted roles"
+        value: "Member, _member_, admin, SwiftOperator"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"
+      - option: "rgw keystone token cache size"
+        value: "{{ keystone_token_cache_size }}"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"
+      - option: "rgw keystone revocation interval"
+        value: "{{ keystone_revocation_interval }}"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"
+      - option: "rgw s3 auth use keystone"
+        value: "{{ rgw_s3_use_keystone }}"
+        apply_to:
+          - "{{ rgw_group_name|default('rgw') }}"

ansible/roles/ceph/tasks/config.yml
@@ -1,0 +1,12 @@
+- name: ensure ceph configurations
+  ini_file:
+    dest: "{{ ceph_conf_file }}"
+    section: "{{ item.0.section }}"
+    option: "{{ item.1.option }}"
+    value: "{{ item.1.value }}"
+  with_subelements:
+    - "{{ ceph_conf }}"
+    - configurations
+  when:
+    - group_names|length > 0
+    - group_names|issubset(item.1.apply_to)

ansible/roles/ceph/tasks/main.yml
@@ -1,4 +1,6 @@
 ---
+- include: config.yml
+
 - include: downgrade_packages.yml
   when: (downgrade_ceph|default('false')|bool)
 

ansible/roles/common/defaults/main.yml
@@ -1,8 +1,4 @@
 ---
-
-fetch_directory: fuel_facts
-fuel_facts_path: /etc/astute.yaml
-
 pkg_install:
   - docker
   - python-docker-py

ansible/roles/common/tasks/main.yml
@@ -16,12 +16,12 @@
 
 - name: fetch fuel astute config
   fetch:
-    src: "{{ fuel_facts_path }}"
-    dest: "{{ fetch_directory }}"
+    src: "{{ fuel_facts_file }}"
+    dest: "{{ fuel_facts_fetch_directory }}"
 
 - name: load fuel astute config
   include_vars:
-    file: "{{ fetch_directory }}/{{ inventory_hostname }}{{ fuel_facts_path}}"
+    file: "{{ fuel_facts_fetch_directory }}/{{ inventory_hostname }}{{ fuel_facts_file}}"
 
 - include: docker.yml
   when: inventory_hostname in groups['controller']

commit 733a1c823b152e7967534825b6c182b1822ac57c

Sync #55 from devel to testing
This is a combination of 4 commits.
This is the 1st commit message:

ansible: fix Ceph configuration updating tasks.

Fixes: ba37679fa2556("ansible: add tasks for Ceph configuration updating.")

Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
(cherry picked from commit 0f52532df6b2f879b1dd57d6a9b174c63a508575)
Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

This is the commit message #2:

ansible: add iptables tasks for trove role.

Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
(cherry picked from commit 36ee3da5147d12165df738b78dcc0013a6a41f57)
Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

This is the commit message #3:

ansible: use keystone v2 endpoint for role trove.

EayunStack is still using keystone v2 endpoint, but keystone ansible
module from OpenStack does not support it. So create a new simple module
based on the original one.

Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
(cherry picked from commit 3ffe8745ab57a4d7b11e3da8d9c970de57039072)
Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

This is the commit message #4:

ansible: allow override rabbit_hosts for trove-guestagent.

Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>
(cherry picked from commit fd5ac70488b05e95ceb4458dfd61388cdd47df76)
Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

ansible/ceph.yml
@@ -32,15 +32,25 @@
         dest: "{{ fuel_facts_fetch_directory }}"
       run_once: True
       delegate_to: "{{ groups['controller'][0] }}"
-      register: fuel_facts_fetched
       when: with_controllers|bool
 
     - name: load fuel facts on obs nodes
       include_vars:
         file: "{{ fuel_facts_fetch_directory }}/{{ groups['controller'][0] }}{{ fuel_facts_file }}"
+      register: obs_load_fuel_facts
       when:
         - with_controllers|bool
         - inventory_hostname in groups['obs']
+
+    - name: set keystone variables for obs nodes
+      set_fact:
+        rgw_use_keystone: True
+        rgw_keystone_url: "http://{{ management_vip }}:{{ keystone_admin_port }}"
+        keystone_admin_token: "{{ keystone.admin_token }}"
+      when:
+        - with_controllers|bool
+        - inventory_hostname in groups['obs']
+        - obs_load_fuel_facts is defined
 
   roles:
     - { role: ceph, action: 'install_packages' }

ansible/group_vars/all.yml
@@ -15,6 +15,7 @@ haproxy_config_base: "/etc/haproxy/conf.d"
 keystone_region: "RegionOne"
 keystone_api_port: "5000"
 keystone_admin_port: "35357"
+keystone_admin_endpoint_v2: "http://{{ management_vip }}:{{ keystone_admin_port }}/v2.0/"
 keystone_admin_endpoint_v3: "http://{{ management_vip }}:{{ keystone_admin_port }}/v3/"
 keystone_internal_endpoint: "http://{{ management_vip }}:{{ keystone_api_port }}/2.0/"
 

ansible/library/keystone_v2_endpoint.py
@@ -1,0 +1,312 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
+#
+# Copyright 2014, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Based on Jimmy Tang's implementation
+
+DOCUMENTATION = """
+---
+module: keystone_v2_endpoint
+short_description:
+    - Manage OpenStack Identity (keystone) v2 endpoint.
+description:
+    - Manage OpenStack Identity (keystone) v2 endpoint.
+      endpoints.
+options:
+    token:
+        description:
+            - The token to be uses in case the password is not specified
+        required: true
+        default: None
+    endpoint:
+        description:
+            - The keystone url for authentication
+        required: true
+    service_name:
+        description:
+            - Name of the service.
+        required: true
+        default: None
+    region_name:
+        description:
+            - Name of the region.
+        required: true
+        default: None
+    service_type:
+        description:
+            - Type of service.
+        required: true
+        default: None
+    endpoint_dict:
+        description:
+            - Dict of endpoint urls to add to keystone for a service
+        required: true
+        default: None
+        type: dict
+    state:
+        description:
+            - Ensuring the endpoint is either present, absent.
+            - It always ensures endpoint is updated to latest url.
+        required: False
+        default: 'present'
+
+requirements: [ python-keystoneclient ]
+"""
+
+EXAMPLES = """
+# Create an endpoint
+- keystone_v2_endpoint:
+    region_name: "RegionOne"
+    service_name: "glance"
+    service_type: "image"
+    endpoint: "http://127.0.0.1:5000/v2.0/"
+    token: "ChangeMe"
+    endpoint_dict:
+      publicurl: "http://127.0.0.1:9292"
+      adminurl: "http://127.0.0.1:9292"
+      internalurl: "http://127.0.0.1:9292"
+
+"""
+
+try:
+    from keystoneclient.v2_0 import client
+except ImportError:
+    keystoneclient_found = False
+else:
+    keystoneclient_found = True
+
+
+class ManageKeystoneV2Endpoint(object):
+    def __init__(self, module):
+        """Manage Keystone via Ansible."""
+        self.state_change = False
+        self.keystone = None
+
+        # Load AnsibleModule
+        self.module = module
+
+    @staticmethod
+    def _facts(facts):
+        """Return a dict for our Ansible facts.
+
+        :param facts: ``dict``  Dict with data to return
+        """
+        return {'keystone_facts': facts}
+
+    def failure(self, error, rc, msg):
+        """Return a Failure when running an Ansible command.
+
+        :param error: ``str``  Error that occurred.
+        :param rc: ``int``     Return code while executing an Ansible command.
+        :param msg: ``str``    Message to report.
+        """
+        self.module.fail_json(msg=msg, rc=rc, err=error)
+
+    def _authenticate(self):
+        """Return a keystone client object."""
+        endpoint = self.module.params.get('endpoint')
+        token = self.module.params.get('token')
+
+        if token is None:
+            self.failure(
+                error='Missing Auth Token',
+                rc=2,
+                msg='Auto token is required!'
+            )
+
+        if token:
+            self.keystone = client.Client(
+                endpoint=endpoint,
+                token=token
+            )
+
+    def _get_service(self, name, srv_type=None):
+        for entry in self.keystone.services.list():
+            if srv_type is not None:
+                if entry.type == srv_type and name == entry.name:
+                    return entry
+            elif entry.name == name:
+                return entry
+        else:
+            return None
+
+    def _get_endpoint(self, region, service_id):
+        """ Getting endpoints per complete definition
+
+        Returns the endpoint details for an endpoint matching
+        region, service id.
+
+        :param service_id: service to which the endpoint belongs
+        :param region: geographic location of the endpoint
+
+        """
+        for entry in self.keystone.endpoints.list():
+            check = [
+                entry.region == region,
+                entry.service_id == service_id,
+            ]
+            if all(check):
+                return entry
+        else:
+            return None
+
+    def _compare_endpoint_info(self, endpoint, endpoint_dict):
+        """ Compare existed endpoint with module parameters
+
+        Return True if public, admin, internal urls are all the same.
+
+        :param endpoint: endpoint existed
+        :param endpoint_dict: endpoint info passed in
+
+        """
+
+        check = [
+            endpoint.adminurl == endpoint_dict.get('adminurl'),
+            endpoint.publicurl == endpoint_dict.get('publicurl'),
+            endpoint.internalurl == endpoint_dict.get('internalurl')
+        ]
+
+        if all(check):
+            return True
+        else:
+            return False
+
+    def ensure_endpoint(self):
+        """Ensures the deletion/modification/addition of endpoints
+        within Keystone.
+
+        Returns the endpoint ID on a successful run.
+
+        """
+        self._authenticate()
+
+        service_name = self.module.params.get('service_name')
+        service_type = self.module.params.get('service_type')
+        region = self.module.params.get('region_name')
+        endpoint_dict = self.module.params.get('endpoint_dict')
+        state = self.module.params.get('state')
+
+        endpoint_dict = {
+            'adminurl': endpoint_dict.get('adminurl', ''),
+            'publicurl': endpoint_dict.get('publicurl', ''),
+            'internalurl': endpoint_dict.get('internalurl', '')
+        }
+
+        service = self._get_service(name=service_name, srv_type=service_type)
+        if service is None:
+            self.failure(
+                error='service [ %s ] was not found.' % service_name,
+                rc=2,
+                msg='Service was not found, does it exist?'
+            )
+
+        existed_endpoint = self._get_endpoint(
+            region=region,
+            service_id=service.id,
+        )
+
+        delete_existed = False
+
+        if state == 'present':
+            ''' Creating an endpoint (if it does
+                not exist) or creating a new one,
+                and then deleting the existing
+                endpoint that matches the service
+                type, name, and region.
+            '''
+            if existed_endpoint is None:
+                self.state_change = True
+                endpoint = self.keystone.endpoints.create(
+                    region=region,
+                    service_id=service.id,
+                    **endpoint_dict
+                )
+            elif existed_endpoint is not None:
+                if not self._compare_endpoint_info(existed_endpoint,
+                                                   endpoint_dict):
+                    self.state_change = True
+                    delete_existed = True
+                else:
+                    endpoint = existed_endpoint
+
+        elif state == 'absent':
+            if existed_endpoint is not None:
+                self.state_change = True
+                delete_existed = True
+
+        if delete_existed:
+            result = self.keystone.endpoints.delete(existed_endpoint)
+            if result[0].status_code != 204:
+                self.module.fail()
+
+        if state != 'absent':
+            facts = self._facts(endpoint.to_dict())
+        else:
+            facts = self._facts({})
+
+        self.module.exit_json(
+            changed=self.state_change,
+            ansible_facts=facts
+        )
+
+# TODO(evrardjp): Deprecate state=update in Q.
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            token=dict(
+                required=True
+            ),
+            endpoint=dict(
+                required=True,
+            ),
+            region_name=dict(
+                required=True
+            ),
+            service_name=dict(
+                required=True
+            ),
+            service_type=dict(
+                required=True
+            ),
+            endpoint_dict=dict(
+                required=True,
+                type='dict'
+            ),
+            state=dict(
+                choices=['present', 'absent'],
+                required=False,
+                default='present'
+            )
+        ),
+        supports_check_mode=False,
+    )
+
+    km = ManageKeystoneV2Endpoint(module=module)
+    if not keystoneclient_found:
+        km.failure(
+            error='python-keystoneclient is missing',
+            rc=2,
+            msg='keystone client was not importable, is it installed?'
+        )
+
+    facts = km.ensure_endpoint()
+
+
+# import module snippets
+from ansible.module_utils.basic import *  # NOQA
+if __name__ == '__main__':
+    main()

ansible/roles/ceph/defaults/main.yml
@@ -24,6 +24,7 @@ ceph_lib_packages:
 ceph_rgw_packages:
   - ceph-radosgw
 
+rgw_use_keystone: "false"
 keystone_accepted_roles: "Member, _member_, admin, SwiftOperator"
 keystone_token_cache_size: "500"
 keystone_revocation_interval: "500"
@@ -31,28 +32,19 @@ rgw_s3_use_keystone: "true"
 
 ceph_conf:
   - section: "global"
+    pre_check: "{{ rgw_use_keystone|bool }}"
+    apply_to:
+      - "{{ rgw_group_name }}"
     configurations:
       - option: "rgw keystone url"
-        value: "http://{{ management_vip }}:{{ keystone_admin_port }}"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"
+        value: "{{ rgw_keystone_url|default('') }}"
       - option: "rgw keystone admin token"
-        value: "{{ keystone['admin_token'] }}"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"
+        value: "{{ keystone_admin_token|default('') }}"
       - option: "rgw keystone accepted roles"
-        value: "Member, _member_, admin, SwiftOperator"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"
+        value: "{{ keystone_accepted_roles }}"
       - option: "rgw keystone token cache size"
         value: "{{ keystone_token_cache_size }}"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"
       - option: "rgw keystone revocation interval"
         value: "{{ keystone_revocation_interval }}"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"
       - option: "rgw s3 auth use keystone"
         value: "{{ rgw_s3_use_keystone }}"
-        apply_to:
-          - "{{ rgw_group_name|default('rgw') }}"

ansible/roles/ceph/tasks/config.yml
@@ -9,4 +9,6 @@
     - configurations
   when:
     - group_names|length > 0
-    - group_names|issubset(item.1.apply_to)
+    - item.0.pre_check|bool
+    - item.0.apply_to == 'all'
+      or group_names|intersect(item.0.apply_to)|length > 0

ansible/roles/trove/defaults/main.yml
@@ -57,12 +57,9 @@ trove_internal_endpoint: "http://{{ management_vip }}:{{ trove_api_port }}/v1.0/
 trove_admin_endpoint: "http://{{ management_vip }}:{{ trove_api_port }}/v1.0/%(tenant_id)s"
 
 trove_endpoints:
-  - url: "{{ trove_public_endpoint }}"
-    interface: "public"
-  - url: "{{ trove_internal_endpoint }}"
-    interface: "internal"
-  - url: "{{ trove_admin_endpoint }}"
-    interface: "admin"
+  publicurl: "{{ trove_public_endpoint }}"
+  internalurl: "{{ trove_internal_endpoint }}"
+  adminurl: "{{ trove_admin_endpoint }}"
 
 trove_log_directory: "/var/log/{{ project_name }}"
 trove_logging_debug: "True"

ansible/roles/trove/tasks/config.yml
@@ -55,3 +55,12 @@
   register: check_trove_containers
   notify:
     - "Restart {{ item.key }} container"
+
+- name: Ensuring iptables for trove
+  iptables:
+    action: insert
+    chain: INPUT
+    protocol: tcp
+    match: multiport
+    destination_port: "{{ trove_api_port }}"
+    jump: ACCEPT

ansible/roles/trove/tasks/main.yml
@@ -1,4 +1,4 @@
-- name: ensure trove tenant, user, endpoints
+- name: ensure trove tenant, user
   run_once: True
   keystone:
     region_name: "{{ keystone_region }}"
@@ -12,14 +12,22 @@
     service_name: "{{ item.service_name | default(None) }}"
     service_type: "{{ item.service_type | default(None) }}"
     description: "{{ item.description | default(None) }}"
-    endpoint_list: "{{ item.endpoint_list | default([]) }}"
   with_items:
     - { command: 'ensure_user' }
     - { command: 'ensure_tenant', tenant: "{{ trove_managed_tenant }}" }
     - { command: 'ensure_user_role', tenant: 'services', role: 'admin' }
     - { command: 'ensure_user_role', tenant: "{{ trove_managed_tenant }}", role: 'admin' }
     - { command: 'ensure_service', service_name: 'trove', service_type: 'database', description: 'Trove Database Service' }
-    - { command: 'ensure_endpoint', service_name: 'trove', service_type: 'database', endpoint_list: "{{ trove_endpoints }}" }
+
+- name: ensure trove endpoint
+  run_once: True
+  keystone_v2_endpoint:
+    region_name: "{{ keystone_region }}"
+    endpoint: "{{ keystone_admin_endpoint_v2 }}"
+    token: "{{ keystone.admin_token }}"
+    service_name: "trove"
+    service_type: "database"
+    endpoint_dict: "{{ trove_endpoints }}"
 
 - name: get id of trove-managed-tenant
   keystone:

ansible/roles/trove/templates/trove-guestagent.conf.j2
@@ -10,7 +10,7 @@ swift_container_allowed_origins = {{ trove_guestagent_conf.default.swift_contain
 guest_log_limit = {{ trove_guestagent_conf.default.guest_log_limit }}
 
 [oslo_messaging_rabbit]
-rabbit_hosts = {% for host in groups['controller'] %}{{ hostvars[host]['ansible_' + hostvars[host]['network_scheme']['roles']['ex']|replace('-', '_')]['ipv4']['address'] }}:{{ rabbit_port }}{% if not loop.last %},{% endif %}{% endfor %}
+rabbit_hosts = {% if trove_guestagent_rabbit_hosts|default([])|length > 0 %}{{ trove_guestagent_rabbit_hosts|join(',') }}{% else %}{% for host in groups['controller'] %}{{ hostvars[host]['ansible_' + hostvars[host]['network_scheme']['roles']['ex']|replace('-', '_')]['ipv4']['address'] }}:{{ rabbit_port }}{% if not loop.last %},{% endif %}{% endfor %}{% endif %}
 
 rabbit_userid = nova
 rabbit_password = {{ rabbit.password }}